d72182c0759a3d8e7368737460466b861a8adeab
SMU-Meadows <62397525+SMU-Meadows@users.noreply.github.com>
2020-12-07 03:54:58 -0600

    added bulk of DHall sub360 pano files

---
+++ js/DHLWaterX.js
@@ -0,0 +1,25 @@
+
+    var DHall = {
+      Image: {
+    xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+    Url: "//SMU-Meadows.github.io/img/panos/DHLwater/",
+    Format: "png",
+    Overlap: "1",
+    TileSize: "256",
+    Size: {
+      Width:  "41618",
+      Height: "14090"
+    },
+    minLevel: 15
+  }
+};
+    
+    var viewer = OpenSeadragon({
+      id: "seadragon-viewer",
+      prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+      tileSources: DHall,
+      showNavigator:  false,
+      visibilityRatio: 1,
+      constrainDuringPan: false,
+      minZoomLevel: 1
+    });
