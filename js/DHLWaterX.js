2bd0a4713856a70b8e4077973a3b33a7568279f8
SMU-Meadows <62397525+SMU-Meadows@users.noreply.github.com>
2020-12-13 22:24:31 -0600

    Update DHLWaterX.js

--- js/DHLWaterX.js
+++ js/DHLWaterX.js
@@ -1,5 +1,5 @@
 
-    var DHalls = {
+    var DHall = {
       Image: {
     xmlns: "http://schemas.microsoft.com/deepzoom/2008",
     Url: "//SMU-Meadows.github.io/img/panos/DHLWater/",
@@ -17,7 +17,7 @@
     var viewer = OpenSeadragon({
       id: "seadragon-viewer",
       prefixUrl: "//openseadragon.github.io/openseadragon/images/",
-      tileSources: DHalls,
+      tileSources: DHall,
       showNavigator:  false,
       visibilityRatio: 1,
       constrainDuringPan: false,
